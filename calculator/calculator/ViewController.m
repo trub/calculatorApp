d515e9368e316389f96d194955d3557900f34f37
matthew weintrub <heytrub@gmail.com>
2015-10-12 12:03:29 -0700

    creates subclasses to implement alcohol calculations for beer,wine and whiskey

--- calculator/calculator/ViewController.m
+++ calculator/calculator/ViewController.m
@@ -10,6 +10,7 @@
 
 @interface ViewController ()
 
+
 @end
 
 @implementation ViewController
@@ -23,5 +24,66 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+- (IBAction)textFieldDidChange:(UITextField *)sender {
+    
+    NSString *enteredText = sender.text;
+    float enteredNumber = [enteredText floatValue];
+    if (enteredNumber == 0) {
+        //the user typed 0, or something that's not a number, so clear the field
+        sender.text = nil;
+    }
+    
+}
+- (IBAction)sliderValueDidChange:(UISlider *)sender {
+    
+    NSLog(@"Slider value changed to %f", sender.value);
+    [self.beerPercentTextField resignFirstResponder];
+    
+}
+- (IBAction)buttonPressed:(id)sender {
+    
+    [self.beerPercentTextField resignFirstResponder];
+    //first, calculate how much alcohol is in all those beers
+    int numberOfBeers = self.beerCountSlider.value;
+    int ouncesInOneBeerGlass = 12; //assume 12oz beer bottles
+    float alcoholPercentageOfBeer = [self.beerPercentTextField.text floatValue] / 100;
+    float ouncesOfAlcoholPerBeer = ouncesInOneBeerGlass * alcoholPercentageOfBeer;
+    float ouncesOfAlocholTotal = ouncesOfAlcoholPerBeer * numberOfBeers;
+    //now, calculate the equivalent amount of wine....
+    float ouncesInOneWineGlass = 5; //wine glasses are usually 5oz
+    float alcoholPercentageOfWine = 0.13; //13% is average
+    float ouncesOfAlcoholPerWineGlass = ouncesInOneWineGlass * alcoholPercentageOfWine;
+    float numberOfWineGlassesForEquivalentAlcoholAmount = ouncesOfAlocholTotal / ouncesOfAlcoholPerWineGlass;
+    //decide whether to use "beer"/"beers" and "glass"/"glasses"
+    NSString *beerText;
+    if (numberOfBeers == 1) {
+        beerText = NSLocalizedString(@"beers", @"singular beer");
+    } else {
+        beerText = NSLocalizedString(@"beer", @"plural of beer");
+    }
+    NSString *wineText;
+    if (numberOfWineGlassesForEquivalentAlcoholAmount == 1){
+        wineText = NSLocalizedString(@"glass", @"singular glass");
+    } else {
+        wineText = NSLocalizedString(@"glasses", @"plural of glass");
+    }
+    //generate the result text, and display it on the label
+    NSString *resultText = [NSString stringWithFormat:NSLocalizedString(@"%d %@ (with %.2f%% alcohol contains as much as %.1f %@ of wine.", nil), numberOfBeers, beerText, [self.beerPercentTextField.text floatValue], numberOfWineGlassesForEquivalentAlcoholAmount, wineText];
+    self.resultLabel.text = resultText;
+    
+    
+}
+- (IBAction)tapGestureDidFire:(UITapGestureRecognizer *)sender {
+    [self.beerPercentTextField resignFirstResponder];
+}
+
+
+
+
+
+
 
 @end
+
+
+
---
+++ calculator/calculator/WhiskeyViewController.h
@@ -0,0 +1,13 @@
+//
+//  WhiskeyViewController.h
+//  calculator
+//
+//  Created by Matthew Weintrub on 10/12/15.
+//  Copyright (c) 2015 matthew weintrub. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface WhiskeyViewController : ViewController
+
+@end
